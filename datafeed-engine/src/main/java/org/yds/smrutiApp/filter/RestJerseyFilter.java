5864e2a78b7caa1f0dfb8095f26eccca562763e2
KPadsala <KPadsala@DEV001.tps.main>
2017-02-23 16:10:51 -0500

    Verifying connection

---
+++ datafeed-engine/src/main/java/org/yds/smrutiApp/filter/RestJerseyFilter.java
@@ -0,0 +1,12 @@
+package org.yds.smrutiApp.filter;
+
+import com.sun.jersey.spi.container.ContainerRequest;
+import com.sun.jersey.spi.container.ContainerRequestFilter;
+
+public class RestJerseyFilter implements ContainerRequestFilter {
+
+	public ContainerRequest filter(ContainerRequest containerRequestContext) {
+		return null;
+	}
+
+}
